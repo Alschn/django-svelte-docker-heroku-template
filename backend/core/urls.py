a4f3055faf24632c3ff9f01444170aa7a6166197
Alschn <67019937+Alschn@users.noreply.github.com>
2024-02-29 13:46:38 +0100

    Add drf-spectacular views for api docs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -13,10 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include('example.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('example.urls'))
 ]
+
+if settings.DEBUG:
+    from core.schema.views import (
+        SchemaAPIView,
+        SchemaSwaggerView,
+        SchemaRedocView
+    )
+
+    urlpatterns += [
+        # openapi schema
+        path('schema/', SchemaAPIView.as_view(), name='schema'),
+        path('schema/swagger/', SchemaSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('schema/redoc/', SchemaRedocView.as_view(url_name='schema'), name='redoc'),
+    ]
